28ac372cc61d333cb7c0d6c2a9e804720a903a21
lok <cleiton_biou@hotmail.com>
2020-08-24 13:59:36 -0300

    err
    Signed-off-by: lok <cleiton_biou@hotmail.com>

--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -1,25 +1,53 @@
-import React from 'react'
+import React, { useState, FormEvent } from 'react'
+
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
 import './styles.css'
 
 export default function Login(){
+
+
+    const [validated, setValidated] = useState(false)
+    const [erros, setErros] = useState({})
+    const [matricula, setMatricula] = useState('')
+    const [senha, setSenha] = useState('')
+
+    function handleSubmit(e: FormEvent){
+        e.preventDefault()        
+    }
+
     return (
         <div id="container">
-            <div className="box">
-                    <h2>Login</h2>
-                <form>
-                    
-                    <label htmlFor="login">Email</label>
-                    <input type="text"/>
-                    <br/>
+            <div className="boxe">
+                    <Form onSubmit={handleSubmit} noValidate validated={validated}>
+                        <h2>Login</h2>
+                        
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Label>Matrícula</Form.Label>
+                            <Form.Control 
+                                type="text" 
+                                value={matricula} 
+                                onChange={(e) => setMatricula(e.target.value)}
+                            />
+                        </Form.Group>
 
-                    <label htmlFor="password">Password</label>
-                    <input type="password"/>
+                        <Form.Group controlId="formBasicPassword">
+                            <Form.Label>Senha</Form.Label>
+                            <Form.Control 
+                                type="password" 
+                                value={senha} 
+                                onChange={(e) => setSenha(e.target.value)}
+                            />
+                        </Form.Group>
+                        <Form.Group controlId="formBasicCheckbox">
+                            <p><a href="">Esqueceu a senha, clique aqui!</a></p>
+                        </Form.Group>
+                        <Button variant="primary" type="submit">
+                            <span>Acessar</span>
+                        </Button>
+                    </Form>
                     
-                    
-                    <p><a href="">Esqueceu a senha, clique aqui!</a></p>
-                    <button type="submit"><span>Acessar</span></button>
-                </form>
             </div>
         </div>
     )
